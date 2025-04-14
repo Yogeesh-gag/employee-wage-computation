bceed59aa405324594ae4582debb77846523813c
Yogi@gag <dr.yogeesh31@gmail.com>
2025-04-14 13:35:21 +0530

    [Yogeesh] Updated: Updated a program by calculating a daily wage of an employee

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,25 +4,38 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class EmployeeWageComputation {
-
-		// Main method
-		public static void main(String[] args) {
+	 // Main method
+    public static void main(String[] args) {
+    	
         // Displaying a welcome message
         System.out.println("Welcome to employee wage computation Problem!!");
-        Scanner sc = new Scanner(System.in);
         
+        // creating a new Scanner Object
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter a employee Name : ");
         String name = sc.nextLine();
         
-        //Creating A Random class object
+        // creating random class object
         Random random = new Random();
-        int attendance = random.nextInt(2);
+        int wagePerHour = 20;
+        int fullDayHours = 8;
         
+        //Taking two digits random number
+        int attendance = random.nextInt(2);
         System.out.println(attendance);
+        
+        // check if the employee is absent
         if (attendance == 0) {
             System.out.println("Employee " + name + " is absent!!");
         } else
             System.out.println("Employee " + name + " is presnt ");
+
+        // calculating daily wage
+        int dailyWage = wagePerHour * fullDayHours;
+        // Output of the daily wage
+        System.out.println("Daily Employee Wage is : " + dailyWage);
+        
+        //Closing the scanner class object
         sc.close();
     }
 	
